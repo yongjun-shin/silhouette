790fbba8775dde82e8485b825dc8e2f3946c2dee
yongjun-shin <syj1634@naver.com>
2023-07-15 04:02:07 +0900

    Feat: 햄버거 메뉴 구현 - 아직 추가작업 필요

--- sil_back/settings.py
+++ sil_back/settings.py
@@ -106,9 +106,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = ( os.path.join('static'), )
+# STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
