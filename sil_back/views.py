e401a5a305439acd71f0cee7dbfc8de01a5ef928
NAME <syj1634@naver.com>
2023-07-26 19:29:38 +0900

    Feat: 회원가입 구현 완료

--- sil_back/views.py
+++ sil_back/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render
 def login_view(request):
     return render(request, 'login.html')
 
-def join_view(request):
-    return render(request, 'join.html')
-
 def mypage_view(request):
     return render(request, 'mypage.html')
